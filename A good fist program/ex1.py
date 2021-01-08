22f4e52541dd8ef7f72fc631d76ad00e74d5aebf
Rishab Mehrotra <rishabmehrotra19@gmail.com>
2021-01-08 09:16:53 +0530

    exercise 1 and exercise 2

---
+++ A good fist program/ex1.py
@@ -0,0 +1,8 @@
+#Run file on terminal with the following command.... python ex1.py
+print("Hellow World!")
+print("Hello Again")
+print("This is fun")
+print("I like typing this")
+print("Yay!... print this.")
+print("6451326545.... I'd much rather you...)()()()()()( 'not'")
+print("this is the ned... oopsi 'end'")
---
+++ Comments/ex2.py
@@ -0,0 +1,7 @@
+#Run file on terminal with the following command.... python ex1.py
+#Single line comment
+print("You can only read this and ot anything else... lol :P")
+"""
+Now i am wirting a multi line comment
+See I can change lines and write whatever i want and still all of this is ignored
+"""
